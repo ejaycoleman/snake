f6f2fb7c06acb7c430ca4fe4722db6842679a894
Elliott Coleman <ejaycoleman@googlemail.com>
2021-06-05 10:23:41 +0100

    grid

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
+interface RowInt {
+  y: number
+}
+
+interface CellInt {
+  x: number
+  y: number
+}
+
+const gridArray: number[] = []
+const gridSize = 35;
+
+for (let i = 0; i <= gridSize; i++) {
+  gridArray.push(i);
+}
+
+const Grid = (): JSX.Element => 
+<div>
+  {gridArray.map(y => 
+    <Row
+      y={y}
+      key={y}
+    />  
+  )}
+</div>
+
+const Row = ({y}: RowInt) => 
+  <div className='grid-row'>
+    {gridArray.map(x => 
+      <Cell
+        x={x}
+        y={y}
+        key={x}
+      />
+    
+    )}
+  </div>
+
+const Cell = ({x, y}: CellInt): JSX.Element => 
+  <div className={isBorder(x, y) ? 'border cell' : 'cell'}/>
+
+const isBorder = (x: number, y: number) => 
+      x === 0 || y === 0 || x === gridSize || y === gridSize
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Grid></Grid>
     </div>
   );
+
+  
 }
+
+
 
 
 export default App;
