3967a7592cf366562194a565910a0008f3bf8c06
Elliott Coleman <ejaycoleman@googlemail.com>
2021-06-06 21:53:03 +0100

    lengthy snake

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react'
 import './App.css';
 
 interface snakeInt {
-  snake: CellInt
+  snake: CellInt[]
 }
 
 interface RowInt {
   y: number,
-  snake: CellInt
+  snake: CellInt[]
 }
 
 interface CellInt {
@@ -18,7 +18,7 @@ interface CellInt {
 interface CellIntWithInfo {
   x: number
   y: number
-  snake: CellInt
+  snake: CellInt[]
 }
 
 interface DirectionInt {
@@ -65,7 +65,7 @@ const Row = ({y, snake}: RowInt) =>
   </div>
 
 const Cell = ({x, y, snake}: CellIntWithInfo): JSX.Element => 
-  <div className={isBorder(x, y) ? 'border cell ' : 'cell ' + (JSON.stringify(snake) === JSON.stringify({x, y}) ? 'snake' : '')}/>
+  <div className={isBorder(x, y) ? 'border cell ' : 'cell ' + (snake.filter(cell => cell.x === x && cell.y === y).length > 0 ? 'snake' : '')}/>
 
 const isBorder = (x: number, y: number) => 
       x === 0 || y === 0 || x === gridSize || y === gridSize
@@ -89,18 +89,21 @@ const direction: DirectionInt = {
   RIGHT: (x: number, y: number) => ({x: x + 1, y})
 }
 
-let currDirection = "UP"
+let currDirection = "DOWN"
 
 const App = (): JSX.Element => {
-  const [snake, setSnake] = useState<CellInt>({x: 0, y: 0});
+  const [snake, setSnake] = useState<CellInt[]>([{x: 3, y: 1}, {x: 2, y: 1}, {x: 1, y: 1}]);
 
-  useEffect(() => 
-    setSnake(randCoord)
-  , [])
+  // useEffect(() => 
+  //   setSnake(randCoord)
+  // , [])
 
   useEffect(() => {
     const onTick = () => {
-      setSnake(direction[currDirection](snake.x, snake.y))
+      const tempSnake = [...snake]
+      tempSnake.pop()
+      tempSnake.unshift(direction[currDirection](tempSnake[0].x, tempSnake[0].y))
+      setSnake(tempSnake)
     };
 
     const interval = setInterval(onTick, 400);
